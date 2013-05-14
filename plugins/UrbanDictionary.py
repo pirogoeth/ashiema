f45fad8fa2fc86beb856c0c7ecb9c1650d2a279b
pirogoeth <pirogoeth@maio.me>
2013-05-14 18:53:22 -0500

    1.0-merge: committing so i can apply stash

--- plugins/UrbanDictionary.py
+++ plugins/UrbanDictionary.py
@@ -7,8 +7,9 @@
 
 import os, logging, core, sys, traceback, re, json, htmlentitydefs
 from core import Plugin, Event, get_connection, util
-from core.util import Escapes as e
+from core.util import Escapes
 from core.util import unescape
+from core.util.texttable import TextTable
 from core.Plugin import Plugin
 from core.HelpFactory import Contexts
 from core.HelpFactory import CONTEXT, PARAMS, DESC, ALIASES
@@ -35,7 +36,7 @@ class UrbanDictionary(Plugin):
             }
         )
         
-        self.url = "http://www.urbandictionary.com/define.php?"
+        self.url = "http://api.urbandictionary.com/v0/define?"
     
     def __deinit__(self):
     
@@ -64,71 +65,67 @@ class UrbanDictionary(Plugin):
             try:
                 term = " ".join(data.message[1:])
             except (IndexError):
-                data.target.message("%s[UrbanDictionary]: %sPlease provide a term to search for." % (e.AQUA, e.BOLD))
+                data.target.message("%s[UrbanDictionary]: %sPlease provide a term to search for." % (Escapes.AQUA, Escapes.BOLD))
                 return
             self.beginQuery(data, term)
         elif data.message == (0, "@ud-cache") or data.message == (0, "urbandictionary-cache"):
             assert self.identification.require_level(data, 1)
-            data.target.message("%s[UrbanDictionary]: %s objects cached." % (e.AQUA, len(self.cache)))
+            data.target.message("%s[UrbanDictionary]: %s objects cached." % (Escapes.AQUA, len(self.cache)))
             return
         elif data.message == (0, "@ud-cache-clear") or data.message == (0, "urbandictionary-cacheclear"):
             assert self.identification.require_level(data, 1)
             self.clean_cache()
-            data.target.message("%s[UrbanDictionary]: Cache forcibly cleared." % (e.LIGHT_BLUE))
+            data.target.message("%s[UrbanDictionary]: Cache forcibly cleared." % (Escapes.LIGHT_BLUE))
             return
     
     def searchUD(self, term):
         
-        _data = urlopen(self.url + "%s" % (
+        data = urlopen(self.url + "%s" % (
             urlencode(
                 {
                     "term" : term
                 }
             )
         ))
-        _data = u'%s' % (_data.read().replace('\n', '\\n'))
-        logging.getLogger("ashiema").error(_data)
-        # _data = json.loads(_data)['string']
-        _data = unescape(_data)
-        if "isn't defined yet" in _data:
-            return (term, term + " is not defined yet.")
-        content = re.compile(r'<div class="definition">(.+?)<\/div><div class="example">(.+?)<\/div>')
-        escapes = re.compile(r"[\r\n\t]")
-        escaped = escapes.sub('', _data.replace("<br/>", '').replace('&quot;', '"').strip())
-        try:
-            _result = content.match(escaped).groups()
-        except: 
-            [logging.getLogger("ashiema").error("%s" % (tb)) for tb in traceback.format_exc(4).split('\n')]
-            result = None
+        data = json.loads(data.read())
 
-        result = [slice for slice in _result]
-        result[0] = result[0].replace("<b>", '').replace("</b>", '').strip()
-        result[1] = result[1].lstrip()
-        result[1] = result[1].replace("<b>", e.BOLD).replace("</b>", e.BOLD)
-
-        return result if result is not None else None
+        return data if data is not None else None
     
     def beginQuery(self, data, term):
     
-        data.target.message("%s[UrbanDictionary]%s: %sSearching..." % (e.BOLD, e.BOLD, e.AQUA))
+        data.target.message("%s[UrbanDictionary]%s: %sSearching..." % (Escapes.BOLD, Escapes.BOLD, Escapes.AQUA))
         try:
-             if term in self.cache:
-                 response = self.cache[term]
-             elif term not in self.cache:
-                 response = self.searchUD(term)
-                 self.cache.update({
-                     term : response
-                 })
-             if response is None:
-                 data.target.message("%s[UrbanDictionary]%s: %s%s%s is not defined yet." % (e.BOLD, e.BOLD, e.RED, term, e.RED))
-                 return
-             else:
-                 _term, _definition = response
-                 data.target.message(
-                     "%s[UrbanDictionary]%s: Definition for %s%s%s:" % (e.BOLD, e.BOLD, e.BOLD, _term, e.BOLD),
-                     "%s[UrbanDictionary]%s: %s%s%s" % (e.BOLD, e.BOLD, e.AQUA, _definition, e.AQUA)
-                 )
-                 return
+            if term in self.cache:
+                response = self.cache[term]
+            elif term not in self.cache:
+                response = self.searchUD(term)
+                self.cache.update({
+                    term : response
+                })
+            if response is None:
+                data.target.message("%s[UrbanDictionary]%s: %s%s%s is not defined yet." % (Escapes.BOLD, Escapes.BOLD, Escapes.RED, term, Escapes.RED))
+                return
+            else:
+                if response['result_type'] == 'no_response':
+                    data.target.message("%s[UrbanDictionary]%s: %s%s%s is not defined yet." % (Escapes.BOLD, Escapes.BOLD, Escapes.RED, term, Escapes.RED))
+                    return
+                definitions = response['list']
+                table = TextTable()
+                table.header = term.title()
+                table.row_size_limit = 45
+                table.add_col_names(['Definition', 'Example'])
+                for definition in definitions:
+                    table.add_row([definition['definition'], definition['example']])
+                    table.add_row([' ', ' '])
+                try:
+                    data.target.privmsg("    ")
+                    for line in str(table).split('\n'):
+                        data.target.privmsg(line)
+                    data.target.privmsg("    ")
+                except (UnicodeEncodeError, UnicodeDecodeError, LookupError) as e:
+                    data.target.privmsg(" - %s%sCould not decode results.%s" % (Escapes.BOLD, Escapes.RED, Escapes.NL))
+                    [logging.getLogger("ashiema").error("%s" % (tb)) for tb in traceback.format_exc(4).split('\n')]
+                    return
         except: [logging.getLogger("ashiema").error("%s" % (tb)) for tb in traceback.format_exc(4).split('\n')]
 
 __data__ = {
