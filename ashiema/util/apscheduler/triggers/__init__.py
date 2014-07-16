564d5cfe70c56769b6a90849c3244b47d87506aa
pirogoeth <pirogoeth@maio.me>
2014-07-15 22:21:16 -0500

    1.1-dev: actually do the proper prune.

--- ashiema/util/apscheduler/triggers/__init__.py
+++
@@ -1,3 +0,0 @@
-from ashiema.util.apscheduler.triggers.cron import CronTrigger
-from ashiema.util.apscheduler.triggers.interval import IntervalTrigger
-from ashiema.util.apscheduler.triggers.simple import SimpleTrigger
